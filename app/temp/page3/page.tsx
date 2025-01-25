de1b44bcee5a3aa245bd0a2cfc8cf9a4d692035f
Vedant363 <vedant.ghumade363@gmail.com>
2025-01-25 22:25:46 +0530

    NoteCraft

--- app/temp/page3/page.tsx
+++ app/temp/page3/page.tsx
@@ -1,19 +1,25 @@
 "use client"
-
-import { useEffect } from 'react'
+import { Suspense, useEffect } from 'react'
 import { useRouter, useSearchParams } from 'next/navigation'
 
-export default function TempPage() {
-  const router = useRouter()
-  const searchParams = useSearchParams()
-  
-  useEffect(() => {
-    const taskId = searchParams.get('taskId')
-    if (taskId) {
-      router.replace(`/tasks/${taskId}`)
-    }
-  }, [router, searchParams])
+function TempPageContent() {
+ const router = useRouter()
+ const searchParams = useSearchParams()
+ 
+ useEffect(() => {
+   const taskId = searchParams.get('taskId')
+   if (taskId) {
+     router.replace(`/tasks/${taskId}`)
+   }
+ }, [router, searchParams])
 
-  return null // This page won't render anything as it immediately redirects
+ return null
 }
 
+export default function TempPage() {
+ return (
+   <Suspense fallback={<div>Loading...</div>}>
+     <TempPageContent />
+   </Suspense>
+ )
+}
